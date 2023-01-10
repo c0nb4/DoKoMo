86f14b8dcf2d167b93791244ba727eba8304de3c
FRehmann <49240249+c0nb4@users.noreply.github.com>
2023-01-10 10:59:35 +0100

    Update models.py
    Added Model for cards

--- doko_mo/player/models.py
+++ doko_mo/player/models.py
@@ -16,3 +16,26 @@ class Player(models.Model):
     def __str__(self) -> str:
         return self.name 
           
+
+class Card(models.Model):
+    SUIT_CHOICES = (
+        ('heart', '♥'),
+        ('diamond', '♦'),
+        ('spade', '♠'),
+        ('club', '♣'),
+    )
+
+    RANK_CHOICES = (
+        ('ace', 'A'),
+        ('king', 'K'),
+        ('queen', 'Q'),
+        ('jack', 'J'),
+        ('ten', '10'),
+        ('nine', '9'),
+    )
+   
+    suit = models.CharField(max_length=7, choices=SUIT_CHOICES)
+    rank = models.CharField(max_length=4, choices=RANK_CHOICES)
+
+    def __str__(self):
+        return self.get_suit_display() + self.get_rank_display()
